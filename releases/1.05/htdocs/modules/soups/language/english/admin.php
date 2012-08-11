02f7312f0b99f8c68afbf6a36edc3880a4b0d9c7
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- releases/1.05/htdocs/modules/soups/language/english/admin.php
+++ releases/1.05/htdocs/modules/soups/language/english/admin.php
@@ -1,37 +1,37 @@
 <?php
 
-	define('_SUP_TEMPLATE_ADMIN_SOUP_DETAILS', 'Compound Soup Information');
-	define('_SUP_TEMPLATE_ADMIN_SOUP_VIEWS', 'Soup User Views:');
-	define('_SUP_TEMPLATE_ADMIN_SOUP_VIEWRUNNERS', 'Soup View Runners:');
-	define('_SUP_TEMPLATE_ADMIN_SOUP_VOTEPERCENTILE', 'Soup Vote:');
-	define('_SUP_TEMPLATE_ADMIN_SOUP_HYPOSISE', 'Hyposise');
-	define('_SUP_TEMPLATE_ADMIN_SOUP_PROCESS', 'Process');
-	define('_SUP_TEMPLATE_ADMIN_SOUP_SYNOPSISE', 'Synopsise');
-	define('_SUP_TEMPLATE_ADMIN_ANNOTATION', 'Reaction Annotation:');
-	define('_SUP_TEMPLATE_ADMIN_WEIGHT', 'Weight:');
-	define('_SUP_TEMPLATE_ADMIN_SYMBOL', 'Symbol:');
-	define('_SUP_MNU_SYMBOL', 'Compound Details');
-	define('_SUP_MNU_ADMIN_HYPOSISE', 'Compound Use Hyposise');
-	define('_SUP_MNU_ADMIN_PROCESS', 'Compound Use Process');
-	define('_SUP_MNU_ADMIN_SYNOPSISE', 'Compound Use Synopsise');
-	define('_SUP_AM_TEMPLATE_ADMIN_COMPOUND', 'Annoted Compound');
-	define('_SUP_TEMPLATE_ADMIN_INFO_DEFINEBY', 'Compound Defined By:');
-	define('_SUP_TEMPLATE_ADMIN_COMPOUND_VIEWS', 'User Views:');
-	define('_SUP_TEMPLATE_ADMIN_COMPOUND_VIEWRUNNERS', 'View Runners:');
-	define('_SUP_TEMPLATE_ADMIN_COMPOUND_VOTEPERCENTILE', 'Compound Vote:');
-	define('_SUP_TEMPLATE_ADMIN_COMPOUND_UNAME', 'User Profile:');
-	define('_SUP_TEMPLATE_ADMIN_COMPOUND_URL', 'User URL:');
-	define('_SUP_TEMPLATE_ADMIN_COMPOUND_HYPOSISE', 'Hyposise');
-	define('_SUP_TEMPLATE_ADMIN_COMPOUND_PROCESS', 'Process');
-	define('_SUP_TEMPLATE_ADMIN_COMPOUND_SYNOPSISE', 'Synopsise');
+	define('_SUP_TEMPLATE_ADMIN_SOUP_DETAILS','Compound Soup Information');
+	define('_SUP_TEMPLATE_ADMIN_SOUP_VIEWS','Soup User Views:');
+	define('_SUP_TEMPLATE_ADMIN_SOUP_VIEWRUNNERS','Soup View Runners:');
+	define('_SUP_TEMPLATE_ADMIN_SOUP_VOTEPERCENTILE','Soup Vote:');
+	define('_SUP_TEMPLATE_ADMIN_SOUP_HYPOSISE','Hyposise');
+	define('_SUP_TEMPLATE_ADMIN_SOUP_PROCESS','Process');
+	define('_SUP_TEMPLATE_ADMIN_SOUP_SYNOPSISE','Synopsise');
+	define('_SUP_TEMPLATE_ADMIN_ANNOTATION','Reaction Annotation:');
+	define('_SUP_TEMPLATE_ADMIN_WEIGHT','Weight:');
+	define('_SUP_TEMPLATE_ADMIN_SYMBOL','Symbol:');
+	define('_SUP_MNU_SYMBOL','Compound Details');
+	define('_SUP_MNU_ADMIN_HYPOSISE','Compound Use Hyposise');
+	define('_SUP_MNU_ADMIN_PROCESS','Compound Use Process');
+	define('_SUP_MNU_ADMIN_SYNOPSISE','Compound Use Synopsise');
+	define('_SUP_AM_TEMPLATE_ADMIN_COMPOUND','Annoted Compound');
+	define('_SUP_TEMPLATE_ADMIN_INFO_DEFINEBY','Compound Defined By:');
+	define('_SUP_TEMPLATE_ADMIN_COMPOUND_VIEWS','User Views:');
+	define('_SUP_TEMPLATE_ADMIN_COMPOUND_VIEWRUNNERS','View Runners:');
+	define('_SUP_TEMPLATE_ADMIN_COMPOUND_VOTEPERCENTILE','Compound Vote:');
+	define('_SUP_TEMPLATE_ADMIN_COMPOUND_UNAME','User Profile:');
+	define('_SUP_TEMPLATE_ADMIN_COMPOUND_URL','User URL:');
+	define('_SUP_TEMPLATE_ADMIN_COMPOUND_HYPOSISE','Hyposise');
+	define('_SUP_TEMPLATE_ADMIN_COMPOUND_PROCESS','Process');
+	define('_SUP_TEMPLATE_ADMIN_COMPOUND_SYNOPSISE','Synopsise');
 
 
-	define('_SUP_TEMPLATE_ADMIN_SOUP_ALLSUBMISSION', 'Soup Submissions:')
-	define('_SUP_TEMPLATE_ADMIN_SOUP_7DAYSSUBMISSION', '7 Day Submissions:')
-	define('_SUP_TEMPLATE_ADMIN_SOUP_14DAYSUBMISSION', '14 Day Submissions:');
-	define('_SUP_TEMPLATE_ADMIN_SOUP_ALIAS', 'Alias:')
-	define('_SUP_TEMPLATE_ADMIN_SOUP_PERIODICALBASE', 'Periodical Base:')
-	define('_SUP_TEMPLATE_ADMIN_SOUP_DEFINEDBY', 'Defined By:');
-	define('_SUP_TEMPLATE_ADMIN_SOUP_CREATED', 'Created:');	
+	define('_SUP_TEMPLATE_ADMIN_SOUP_ALLSUBMISSION','Soup Submissions:')
+	define('_SUP_TEMPLATE_ADMIN_SOUP_7DAYSSUBMISSION','7 Day Submissions:')
+	define('_SUP_TEMPLATE_ADMIN_SOUP_14DAYSUBMISSION','14 Day Submissions:');
+	define('_SUP_TEMPLATE_ADMIN_SOUP_ALIAS','Alias:')
+	define('_SUP_TEMPLATE_ADMIN_SOUP_PERIODICALBASE','Periodical Base:')
+	define('_SUP_TEMPLATE_ADMIN_SOUP_DEFINEDBY','Defined By:');
+	define('_SUP_TEMPLATE_ADMIN_SOUP_CREATED','Created:');	
 
 ?>
