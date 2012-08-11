dd0ff7844148b0fa675ce27c7c9fc1ddba8fd67b
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- XoopsModules/soups/releases/1.05/htdocs/modules/soups/language/english/modinfo.php
+++ XoopsModules/soups/releases/1.05/htdocs/modules/soups/language/english/modinfo.php
@@ -1,25 +1,25 @@
 <?php
 	
 	// xoops version module data
-	define('_SUP_MI_NAME', 'Compound Soups');
-	define('_SUP_MI_DESC', 'Compound Soups for the budding chemist as - <em>chemist don\'t have blogs they have soups!</em>');
+	define('_SUP_MI_NAME','Compound Soups');
+	define('_SUP_MI_DESC','Compound Soups for the budding chemist as - <em>chemist don\'t have blogs they have soups!</em>');
 
 	// messages
-	define('_SUP_ERROR_COMPOUND_MODULE', 'You are required to have compounds module installed before installing Soups 1.x.x for Compounds 2.18 or later!');
+	define('_SUP_ERROR_COMPOUND_MODULE','You are required to have compounds module installed before installing Soups 1.x.x for Compounds 2.18 or later!');
 
 	// admin menus
-	define('_SUP_AM_SOUPINDEX', 'Main Soups Index');
-	define('_SUP_AM_SOUPLIST', 'Chemical Soup List');
-	define('_SUP_AM_SOUPNEW', 'New Chemical Soup');
-	define('_SUP_AM_COMPOUNDS', 'New Compounds List');	
+	define('_SUP_AM_SOUPINDEX','Main Soups Index');
+	define('_SUP_AM_SOUPLIST','Chemical Soup List');
+	define('_SUP_AM_SOUPNEW','New Chemical Soup');
+	define('_SUP_AM_COMPOUNDS','New Compounds List');	
 	
 	// menus
-	define('_SUP_MI_LISTALL', 'List all Soups');
-	define('_SUP_MI_7DAY', '7 Day Soups');
-	define('_SUP_MI_14DAY', '14 Day Soups');
+	define('_SUP_MI_LISTALL','List all Soups');
+	define('_SUP_MI_7DAY','7 Day Soups');
+	define('_SUP_MI_14DAY','14 Day Soups');
 	
 	// preferences
-	define('_SUP_MI_COMPONENTS', 'Maximum Soup Components');
-	define('_SUP_MI_COMPONENTS_DESC', 'This is the maximum number of compounds you can added to each soup.');
+	define('_SUP_MI_COMPONENTS','Maximum Soup Components');
+	define('_SUP_MI_COMPONENTS_DESC','This is the maximum number of compounds you can added to each soup.');
 	
 ?>
